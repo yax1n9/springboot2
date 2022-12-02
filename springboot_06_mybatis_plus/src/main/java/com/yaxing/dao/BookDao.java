b489d3a1f5bc4d5569af716b56684cf22aee697d
yax1n9 <1904512816@qq.com>
2022-12-02 12:34:59 +0800

    整合mybatis-plus

---
+++ springboot_06_mybatis_plus/src/main/java/com/yaxing/dao/BookDao.java
@@ -0,0 +1,14 @@
+package com.yaxing.dao;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yaxing.domain.Book;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author yx
+ * @date 2022/12/2
+ */
+@Mapper
+public interface BookDao extends BaseMapper<Book> {
+}
