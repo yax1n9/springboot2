336bcefca2dfd4ec49f5040dd4ee7496f9d517ad
yax1n9 <1904512816@qq.com>
2022-12-02 09:46:48 +0800

    junit测试类与启动类不在同一包或子包下解决方法

--- springboot_04_junit/src/test/java/com/yaxing/Springboot04JunitApplicationTests.java
+++ springboot_04_junit/src/test/java/com/yaxing/Springboot04JunitApplicationTests.java
@@ -3,8 +3,13 @@ package com.yaxing;
 import com.yaxing.dao.BookDao;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
+// 当测试类与启动类不在同一包或启动类的子包下时，使用下面方式其一可以解决
+// @SpringBootTest(classes = Springboot04JunitApplicationTests.class)
+// @ContextConfiguration(classes = Springboot04JunitApplicationTests.class)
 @SpringBootTest
 class Springboot04JunitApplicationTests {
     // 1. 导入需要测试的对象
