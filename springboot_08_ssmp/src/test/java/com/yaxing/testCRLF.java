045222bc92a326f546b565e64a417c1cff382ed9
yax1n9 <1904512816@qq.com>
2022-12-05 09:19:41 +0800

    测试

---
+++ springboot_08_ssmp/src/test/java/com/yaxing/testCRLF.java
@@ -0,0 +1,11 @@
+package com.yaxing;
+
+/**
+ * @author yx
+ * @date 2022/12/5
+ */
+public class testCRLF {
+    public void test() {
+        System.out.println("test CRLF --> LF, auto");
+    }
+}
